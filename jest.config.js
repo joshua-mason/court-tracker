7c52ecad7da642b8bdf5e79eae69206400cf503b
Joshua Mason <jmmason95@googlemail.com>
2025-08-02 01:49:08 +0100

    add github actions CI test

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,5 @@ export default {
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
-  },
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/main.ts', // Skip main.ts as it's mostly orchestration
-    '!**/*.d.ts'
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html']
+  }
 };
